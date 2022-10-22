b6e3305c9ece3094cf7b7ed8877d8f9057da8e2c
Athfan Khaleel <athphane@gmail.com>
2022-10-22 13:02:42 +0500

    move deezer stuff to its own file

---
+++ deezer_interface.py
@@ -0,0 +1,49 @@
+from time import sleep
+
+from fake_useragent import UserAgent
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+field_ids = {
+    'gdpr': 'gdpr-btn-accept-all',
+    'username': 'login_mail',
+    'password': 'login_password',
+    'button': 'login_form_submit'
+}
+
+
+def get_arl(deezer):
+    options = Options()
+    ua = UserAgent()
+    user_agent = ua.random
+    options.add_argument(f'user-agent={user_agent}')
+    options.add_argument('--headless')
+
+    driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/local/bin/chromedriver')
+
+    driver.get(deezer['login_url'])
+
+    sleep(5)
+
+    driver.find_element(value=field_ids['gdpr']).click()
+
+    sleep(2)
+
+    driver.find_element(value=field_ids['username']).send_keys(deezer['username'])
+    driver.find_element(value=field_ids['password']).send_keys(deezer['password'])
+
+    sleep(2)
+
+    driver.find_element(value=field_ids['button']).click()
+
+    sleep(5)
+
+    cookies = driver.get_cookies()
+
+    for x in cookies:
+        if x['name'] == 'arl':
+            driver.quit()
+            return x['value']
+
+    driver.quit()
+    return None
--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import json
 from configparser import ConfigParser
-from time import sleep
 
-import chromedriver_autoinstaller
 import requests
-from fake_useragent import UserAgent
 from fastapi import FastAPI
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-chromedriver_autoinstaller.install()
+from deezer_interface import get_arl
 
 config_file = 'config.ini'
 config = ConfigParser()
@@ -19,49 +14,11 @@ bot_token = config.get('telegram', 'bot_token')
 telegram_api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 chat_id = config.get('telegram', 'chat_id')
 
-field_ids = {
-    'gdpr': 'gdpr-btn-accept-all',
-    'username': 'login_mail',
-    'password': 'login_password',
-    'button': 'login_form_submit'
-}
-
 deezer = {
     'login_url': 'https://www.deezer.com/en/login',
     'username': config.get('deezer', 'username'),
     'password': config.get('deezer', 'password'),
 }
-
-
-def get_arl():
-    options = Options()
-    ua = UserAgent()
-    userAgent = ua.random
-    options.add_argument(f'user-agent={userAgent}')
-    options.add_argument('--headless')
-
-    driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/local/bin/chromedriver')
-
-    driver.get(deezer['login_url'])
-
-    sleep(5)
-    driver.find_element(value=field_ids['gdpr']).click()
-    sleep(2)
-    driver.find_element(value=field_ids['username']).send_keys(deezer['username'])
-    driver.find_element(value=field_ids['password']).send_keys(deezer['password'])
-    sleep(2)
-    driver.find_element(value=field_ids['button']).click()
-    sleep(5)
-
-    cookies = driver.get_cookies()
-
-    for x in cookies:
-        if x['name'] == 'arl':
-            driver.quit()
-            return x['value']
-
-    driver.quit()
-    return None
 
 
 def send_telegram_notification(authorization):
@@ -87,7 +44,7 @@ def read_item(app_id: int, token: str):
 
     for x in authorizations:
         if x['app_id'] == app_id and x['token'] == token:
-            arl = get_arl()
+            arl = get_arl(deezer)
             if arl is not None:
                 send_telegram_notification(x)
                 return {
